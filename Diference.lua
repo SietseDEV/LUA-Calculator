22f568809a71d8742e1011ba7c1c8996ead82c9c
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2022-09-10 18:58:58 +0200

    Help File
    this file will help people that dont understand it

---
+++ Diference.lua
@@ -0,0 +1,18 @@
+--[[
+
+the diference is the programming type uses: + - * /
+and the school type uses: + - x ;
+
+programming type:
++ does number1 and number2 together
+- deletes number2 in number1
+* does number 1  + so many times number2 said
+/ removes number1 in number1 as many times number 2 said
+
+school type:
++ does number1 and number2 together
+- deletes number2 in number1
+x does number 1  + so many times number2 said
+; removes number1 in number1 as many times number 2 said
+
+--]]
